dfbb468a19e7e408d754711c0b90aa5f7dd34be2
YJ <yedoong919@gmail.com>
2023-07-09 00:08:47 +0900

    [ADD/MOD] TestController 수정 및 관련 Class/Interface/DAO 추가

--- src/main/java/me/yunyeji/springbootdeveloper/TestController.java
+++ src/main/java/me/yunyeji/springbootdeveloper/TestController.java
@@ -1,13 +1,21 @@
 package me.yunyeji.springbootdeveloper;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class TestController {
 
+    // TestService Bean 주입
+    @Autowired
+    TestService testService;
+
     @GetMapping("/test")
-    public String test() {
-        return "Hello World!";
+    public List<Member> getAllMembers() {
+        List<Member> members = testService.getAllMembers();
+        return members;
     }
 }
